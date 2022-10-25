a7864661e8926d9af0288a4de3e4093b76a208b8
Vikas Uttamrao Thombare <109624341+vikas16042019@users.noreply.github.com>
2022-10-25 14:58:02 +0530

    didďminor changes

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate= document.querySelector("#btn-Translate");
+
+btnTranslate.addEventListener("click", function clickevent()
+{
+    console.log("Click Succesfull");
+});
+
+console.log(btnTranslate);

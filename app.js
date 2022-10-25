703f9f2f9d297116e2dcfa837157d7a5a5e26e56
Vikas Uttamrao Thombare <109624341+vikas16042019@users.noreply.github.com>
2022-10-25 20:07:01 +0530

    trying to figure out
    sorting and finding error

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var btnTranslate= document.querySelector("#btn-Translate");
 var txtInput= document.querySelector("#txt-input");
-
+var outputDiv= document.querySelector("#output");
 btnTranslate.addEventListener("click", clickevent )
+
+var userURl= "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getURL(text)
+{
+    return userURl+ "?" +"text=" + text;
+}
+
+
+function errorHandler(error)
+{
+    console.log("Error Occured",error);
+    alert("Something Went Wrong !!! Try again later ");
+}
 
 function clickevent()
 {
-    console.log("Clicked");
-    console.log("Input", txtInput.value);
+    var inputText= txtInput.value;
+    fetch(getURL(inputText))
+    .then(response => response.json)
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
 }
